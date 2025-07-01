91c48a3fff0a50bbc42caf3dc1d9f1987ca24f07
awahyudi1 <167944609+awahyudi1@users.noreply.github.com>
2025-07-01 13:54:21 +0700

    setup app, file and folder structure, routing and auth pages

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -5,71 +5,105 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
+import FormField from "@/components/formField";
 import Image from "next/image";
+import Link from "next/link";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-const formSchema = z.object({
-	username: z.string().min(2, {
-		message: "Username must be at least 2 characters.",
-	}),
-});
+const authFormSchema = (type: FormType) => {
+	return z.object({
+		name: type === "sign-up" ? z.string().min(3) : z.string().optional(),
+		email: z.string().email(),
+		password: z.string().min(3),
+	});
+};
 
-const AuthForm = () => {
+const AuthForm = ({ type }: { type: FormType }) => {
+	const router = useRouter();
+	const formSchema = authFormSchema(type);
+
 	// 1. Define your form.
 	const form = useForm<z.infer<typeof formSchema>>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
-			username: "",
+			name: "",
+			email: "",
+			password: "",
 		},
 	});
-
 	// 2. Define a submit handler.
+	const isSignIn = type === "sign-in";
 	function onSubmit(values: z.infer<typeof formSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
-		console.log(values);
+		try {
+			if (type === "sign-up") {
+				toast.success("Sign up successful!");
+				router.push("/sign-in");
+			} else {
+				toast.success("Sign in successful!");
+				router.push("/");
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(`An error occurred: ${error}`);
+		}
 	}
 
 	return (
 		<div className="card-border lg:min-w-[566px]">
-			<div className="flex-flex-col gap-6 card py-14 px-10">
+			<div className="flex flex-col gap-6 card py-14 px-10">
 				<div className="flex flex-row gap-2 justify-center">
 					<Image src="/logo.svg" alt="logo" width={38} height={32} />
 					<h2 className="text-primary-100">PrepWise</h2>
 				</div>
-			</div>
-			<Form {...form}>
-				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-					<FormField
-						control={form.control}
-						name="username"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Username</FormLabel>
-								<FormControl>
-									<Input placeholder="shadcn" {...field} />
-								</FormControl>
-								<FormDescription>
-									This is your public display name.
-								</FormDescription>
-								<FormMessage />
-							</FormItem>
+				<h3> Practice Jobs Interview With AI</h3>
+
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="w-full space-y-6 mt-4 form "
+					>
+						{!isSignIn && (
+							<FormField
+								control={form.control}
+								name="name"
+								label="Name"
+								placeholder="Your Name"
+							/>
 						)}
-					/>
-					<Button type="submit">Submit</Button>
-				</form>
-			</Form>
+						<FormField
+							control={form.control}
+							name="email"
+							label="Email"
+							placeholder="Your Email Address"
+							type="email"
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							label="Password"
+							placeholder="Enter Your Password"
+							type="password"
+						/>
+
+						<Button className="btn" type="submit">
+							{isSignIn ? "Sign In" : "Create An Account"}
+						</Button>
+					</form>
+				</Form>
+
+				<p className="text-center">
+					{isSignIn ? "Don't have an account?" : "Already have an account?"}
+					<Link
+						href={!isSignIn ? "/sign-in" : "/sign-up"}
+						className="font-bold text-user-primary ml-1"
+					>
+						{!isSignIn ? "Sign In" : "Sign Up"}
+					</Link>
+				</p>
+			</div>
 		</div>
 	);
 };
-
 export default AuthForm;
